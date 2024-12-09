1676ceeee294de5db98fc46728924cd65dbe0f6f
Alfiia Barylenko <98765399+Heshercat@users.noreply.github.com>
2024-12-09 17:32:04 +0200

    Update main.tf

--- tf-s3_vsc-test/main.tf
+++ tf-s3_vsc-test/main.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "global/s3/terraform.tfstate"  # Path in S3 where the state file will be stored
     region         = "us-west-2"
     encrypt        = true                           # Enables server-side encryption
-    dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
+    # dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
   }
 }
 
